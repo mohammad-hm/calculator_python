3d1c08092fc5e62e86fd22ee70ee9145fe61277b
mohammad hajimohammadi <mohammad.hm1371@yahoo.com>
2021-04-29 23:24:38 +0430

    entry control set

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import tkinter
 from tkinter import *
+import re
 
 root = Tk()
 root.title("Calculator")
 operator = ""
-inputNum = StringVar()
+
+
+def callback(input_num):
+    if input_num.isdigit():
+        print(input_num)
+        return True
+
+    elif input_num == "":
+        print(input_num)
+        return True
+
+    else:
+        print(input_num)
+        return False
+
 
 root.geometry('300x400')
 root.minsize(200, 300)
@@ -19,9 +35,11 @@ Grid.rowconfigure(root, 3, weight=1)
 Grid.columnconfigure(root, 3, weight=1)
 Grid.rowconfigure(root, 4, weight=1)
 
-display = Entry(root, textvariable=inputNum, insertwidth=3, bg='white', justify='left').grid(row=0, column=0, rowspan=1,
-                                                                                             columnspan=4,
-                                                                                             sticky="NSWE")
+reg = root.register(callback)
+display = Entry(root, insertwidth=3, bg='white', justify='left')
+display.grid(row=0, column=0, rowspan=1, columnspan=4, sticky="NSWE")
+display.config(validate="key", validatecommand=(reg, '%P'))
+
 
 btn7 = Button(root, text="7", fg='red', font=("arial", 15, "bold")).grid(row=1, column=0, sticky="NSEW")
 btn8 = Button(root, text="8", fg='red', font=("arial", 15, "bold")).grid(row=1, column=1, sticky="NSEW")
@@ -39,5 +57,6 @@ cls = Button(root, text="C", fg='red', font=("arial", 15, "bold")).grid(row=4, c
 btn0 = Button(root, text="0", fg='red', font=("arial", 15, "bold")).grid(row=4, column=1, sticky="NSEW")
 equal = Button(root, text="=", fg='red', font=("arial", 15, "bold")).grid(row=4, column=2, sticky="NSEW")
 division = Button(root, text="/", fg='red', font=("arial", 15, "bold")).grid(row=4, column=3, sticky="NSEW")
+
 
 root.mainloop()
